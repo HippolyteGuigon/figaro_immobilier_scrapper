899d8e2a1b3bd006cc31ea820e15b7da6d332e3e
HippolyteGuigon <hippolyte.guigon@polytechnique.edu>
2022-10-02 20:49:07 +0200

    push_avec_un_black

--- logs/logs_config.py
+++ logs/logs_config.py
@@ -1,19 +1,20 @@
 import logging
 import sys
 
+
 def main():
 
     logger = logging.getLogger()
     logger.setLevel(logging.INFO)
-    formatter = logging.Formatter('%(asctime)s | %(levelname)s | %(message)s')
+    formatter = logging.Formatter("%(asctime)s | %(levelname)s | %(message)s")
 
     stdout_handler = logging.StreamHandler(sys.stdout)
     stdout_handler.setLevel(logging.DEBUG)
     stdout_handler.setFormatter(formatter)
 
-    file_handler = logging.FileHandler('/Users/hippolyteguigon/se_loger_scrapper/logs.log', mode='a')
-
+    file_handler = logging.FileHandler(
+        "/Users/hippolyteguigon/se_loger_scrapper/logs.log", mode="a"
+    )
 
     logger.addHandler(file_handler)
     logger.addHandler(stdout_handler)
-
--- scrapper/scrapper.py
+++ scrapper/scrapper.py
@@ -1,29 +1,35 @@
 from selenium import webdriver
-import warnings 
+import warnings
+
 warnings.filterwarnings("ignore")
 options = webdriver.ChromeOptions()
-options.add_argument('headless')
+options.add_argument("headless")
 options.add_argument("start-maximized")
 from selenium.webdriver.chrome.service import Service
 from webdriver_manager.chrome import ChromeDriverManager
-import sys 
+import sys
 from logs import logs_config
 import logging
 
 url = "https://immobilier.lefigaro.fr/"
 
-class Scrapper:
 
+class Scrapper:
     def __init__(self):
         logs_config.main()
 
     def connect(self):
         url = "https://immobilier.lefigaro.fr/"
-        s=Service(ChromeDriverManager(log_level=0).install())
-        driver = webdriver.Chrome(service=s,options=options)
+        s = Service(ChromeDriverManager(log_level=0).install())
+        driver = webdriver.Chrome(service=s, options=options)
         driver.maximize_window()
         driver.get(url)
 
-        #Trouver un moyen plus élégant de vérifier la connexion 
-        if driver.find_element_by_xpath('/html/body/div[1]/div/div/main/section[1]/div/div[2]/div/input').get_attribute('placeholder')=="Saisissez une ou plusieurs villes":
-            logging.warning('La connexion a bien réussie')
+        # Trouver un moyen plus élégant de vérifier la connexion
+        if (
+            driver.find_element_by_xpath(
+                "/html/body/div[1]/div/div/main/section[1]/div/div[2]/div/input"
+            ).get_attribute("placeholder")
+            == "Saisissez une ou plusieurs villes"
+        ):
+            logging.warning("La connexion a bien réussie")
