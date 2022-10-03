c184988eeeee39da1a1590aa28396f365ce2f6c6
HippolyteGuigon <hippolyte.guigon@polytechnique.edu>
2022-10-03 09:10:01 +0200

    push_with_good_logs

--- scrapper/scrapper.py
+++ scrapper/scrapper.py
@@ -32,6 +32,7 @@ class Scrapper:
             logging.info("La connexion à la page d'acceuil a bien réussie")
 
     def search_type(self,choice):
+        self.connect()
         choice = str(choice).lower()
         assert choice in ["acheter","louer"], "L'utilisateur doit choisir entre acheter et louer un bien"
         logging.info(f"L'utilisateur a choisi l'otion {choice}")
@@ -45,8 +46,8 @@ class Scrapper:
 
         search_button = driver.find_element_by_xpath('//*[@id="homepage-v2"]/section[1]/div/button[2]')
         driver.execute_script("arguments[0].click();", search_button)
-
-        if "Enregistrer cette recherche" in driver.page_source:
+        driver.implicitly_wait(3)
+        if "Créer une alerte" in driver.page_source:
             logging.info("La recherche a bien aboutie")
         else:
             logging.info("La recherche n'a pas aboutie")
