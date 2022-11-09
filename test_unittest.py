8bf04aa7ca88a7cb09853bd3f5791e9a3ef1c65a
HippolyteGuigon <hippolyte.guigon@polytechnique.edu>
2022-11-09 02:06:19 +0100

    pushing_new_test_version

--- test_unittest.py
+++ test_unittest.py
@@ -31,26 +31,29 @@ class Test(unittest.TestCase):
         filter.check_connect()
         filter.search_type("louer")
 
-        result_filter_ville=filter.filter_search(["Paris","Lyon 12"])
-        result_filter_surface=filter.filter_surface(10, 50)
-        result_filter_price=filter.filter_price(10, 3000)
+        result_filter_ville = filter.filter_search(["Paris", "Lyon 12"])
+        result_filter_surface = filter.filter_surface(10, 50)
+        result_filter_price = filter.filter_price(10, 3000)
 
         if "Le filtrage a bien été opéré" in result_filter_ville:
-            bool_result_ville=True
+            bool_result_ville = True
         else:
-            bool_result_ville=False
+            bool_result_ville = False
 
         if "L'utilisateur a filtré les prix entre" in result_filter_price:
-            bool_result_price=True
+            bool_result_price = True
         else:
-            bool_result_price=False
+            bool_result_price = False
 
         if "L'utilisateur a filtré la surface entre" in result_filter_surface:
-            bool_result_surface=True
+            bool_result_surface = True
         else:
-            bool_result_surface=False
+            bool_result_surface = False
 
-        self.assertTrue(all([bool_result_ville,bool_result_price,bool_result_surface]))
+        self.assertTrue(
+            all([bool_result_ville, bool_result_price, bool_result_surface])
+        )
+
 
 if __name__ == "__main__":
     unittest.main()
